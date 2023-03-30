1d14ed7eb905a55e2d3baaf5909636e7d84cb8ad
sudago <sudago22@gmail.com>
2023-03-30 14:16:25 +0900

    Test 파일

--- src/test/java/jian/springcafe/SpringCafeApplicationTests.java
+++ src/test/java/jian/springcafe/SpringCafeApplicationTests.java
@@ -1,10 +1,10 @@
-package kr.codesqaud.cafe;
+package jian.springcafe;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class CafeApplicationTests {
+class SpringCafeApplicationTests {
 
 	@Test
 	void contextLoads() {

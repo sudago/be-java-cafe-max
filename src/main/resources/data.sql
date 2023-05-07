962802201b521adad0c8c2c621e2bf35d88f095f
sudago <sudago22@gmail.com>
2023-05-08 01:39:47 +0900

    docs: 페이징 기능 작동 확인을 위한 게시글 대량 추가

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,7 +1,7 @@
 INSERT INTO article(writer, title, contents, created_at, points, deleted)
-VALUES ('jianId', '안뇽! 첫 게시글 이에요!', '헤헤 게시글 성공!', '20230413', '2', 'false');
+VALUES ('jianId', '안뇽! 첫 게시글 이에요!', '헤헤 게시글 성공!', '20230401', '1', 'false');
 INSERT INTO article(writer, title, contents, created_at, points, deleted)
-VALUES ('yukiId', '나능!! 유키당!!', '에오🐱', '20230413', '30', 'false');
+VALUES ('yukiId', '나능!! 유키당!!', '에오🐱', '20230402', '30', 'false');
 
 INSERT INTO reply(writer, contents, created_at, article_id, deleted)
 VALUES ('jianId', '댓글도 성공!', '20230413', '1', 'false');
@@ -12,3 +12,155 @@ INSERT INTO users(user_id, password, name, email, deleted)
 VALUES ('jianId', '1234', 'jian', 'jian@gmail.com', 'false');
 INSERT INTO users(user_id, password, name, email, deleted)
 VALUES ('yukiId', '1234', 'yuki', 'yuki@gmail.com', 'false');
+
+-- 페이징 테스트
+INSERT INTO article(writer, title, contents, created_at, points, deleted)
+VALUES ('jianId', '3', '', '20230403', '3', 'false');
+INSERT INTO article(writer, title, contents, created_at, points, deleted)
+VALUES ('jianId', '4', '', '20230404', '4', 'false');
+INSERT INTO article(writer, title, contents, created_at, points, deleted)
+VALUES ('jianId', '5', '', '20230405', '5', 'false');
+INSERT INTO article(writer, title, contents, created_at, points, deleted)
+VALUES ('jianId', '6', '', '20230406', '6', 'false');
+INSERT INTO article(writer, title, contents, created_at, points, deleted)
+VALUES ('jianId', '7', '', '20230407', '7', 'false');
+INSERT INTO article(writer, title, contents, created_at, points, deleted)
+VALUES ('jianId', '8', '', '20230408', '8', 'false');
+INSERT INTO article(writer, title, contents, created_at, points, deleted)
+VALUES ('jianId', '9', '', '20230409', '9', 'false');
+INSERT INTO article(writer, title, contents, created_at, points, deleted)
+VALUES ('jianId', '10', '', '20230410', '10', 'false');
+INSERT INTO article(writer, title, contents, created_at, points, deleted)
+VALUES ('jianId', '11', '', '20230411', '11', 'false');
+INSERT INTO article(writer, title, contents, created_at, points, deleted)
+VALUES ('jianId', '12', '', '20230412', '12', 'false');
+INSERT INTO article(writer, title, contents, created_at, points, deleted)
+VALUES ('jianId', '13', '', '20230413', '13', 'false');
+INSERT INTO article(writer, title, contents, created_at, points, deleted)
+VALUES ('jianId', '14', '', '20230414', '14', 'false');
+INSERT INTO article(writer, title, contents, created_at, points, deleted)
+VALUES ('jianId', '15', '', '20230415', '15', 'false');
+INSERT INTO article(writer, title, contents, created_at, points, deleted)
+VALUES ('jianId', 'title', '', '20230416', '16', 'false');
+INSERT INTO article(writer, title, contents, created_at, points, deleted)
+VALUES ('jianId', 'title', '', '20230417', '17', 'false');
+INSERT INTO article(writer, title, contents, created_at, points, deleted)
+VALUES ('jianId', 'title', '', '20230418', '18', 'false');
+INSERT INTO article(writer, title, contents, created_at, points, deleted)
+VALUES ('jianId', 'title', '', '20230419', '19', 'false');
+INSERT INTO article(writer, title, contents, created_at, points, deleted)
+VALUES ('jianId', 'title', '', '20230420', '20', 'false');
+INSERT INTO article(writer, title, contents, created_at, points, deleted)
+VALUES ('jianId', 'title', '', '20230421', '21', 'false');
+INSERT INTO article(writer, title, contents, created_at, points, deleted)
+VALUES ('jianId', 'title', '', '20230422', '22', 'false');
+INSERT INTO article(writer, title, contents, created_at, points, deleted)
+VALUES ('jianId', 'title', '', '20230423', '23', 'false');
+INSERT INTO article(writer, title, contents, created_at, points, deleted)
+VALUES ('jianId', 'title', '', '20230424', '24', 'false');
+INSERT INTO article(writer, title, contents, created_at, points, deleted)
+VALUES ('jianId', 'title', '', '20230425', '25', 'false');
+INSERT INTO article(writer, title, contents, created_at, points, deleted)
+VALUES ('jianId', 'title', '', '20230426', '26', 'false');
+INSERT INTO article(writer, title, contents, created_at, points, deleted)
+VALUES ('jianId', 'title', '', '20230427', '27', 'false');
+INSERT INTO article(writer, title, contents, created_at, points, deleted)
+VALUES ('jianId', 'title', '', '20230428', '28', 'false');
+INSERT INTO article(writer, title, contents, created_at, points, deleted)
+VALUES ('jianId', 'title', '', '20230429', '29', 'false');
+INSERT INTO article(writer, title, contents, created_at, points, deleted)
+VALUES ('jianId', 'title', '', '20230430', '30', 'false');
+INSERT INTO article(writer, title, contents, created_at, points, deleted)
+VALUES ('jianId', 'title', '', '20230501', '31', 'false');
+INSERT INTO article(writer, title, contents, created_at, points, deleted)
+VALUES ('jianId', 'title', '', '20230502', '32', 'false');
+INSERT INTO article(writer, title, contents, created_at, points, deleted)
+VALUES ('jianId', 'title', '', '20230503', '33', 'false');
+INSERT INTO article(writer, title, contents, created_at, points, deleted)
+VALUES ('jianId', 'title', '', '20230504', '34', 'false');
+INSERT INTO article(writer, title, contents, created_at, points, deleted)
+VALUES ('jianId', 'title', '', '20230505', '35', 'false');
+INSERT INTO article(writer, title, contents, created_at, points, deleted)
+VALUES ('jianId', 'title', '', '20230506', '36', 'false');
+INSERT INTO article(writer, title, contents, created_at, points, deleted)
+VALUES ('jianId', 'title', '', '20230507', '37', 'false');
+INSERT INTO article(writer, title, contents, created_at, points, deleted)
+VALUES ('jianId', 'title', '', '20230508', '38', 'false');
+INSERT INTO article(writer, title, contents, created_at, points, deleted)
+VALUES ('jianId', 'title', '', '20230509', '39', 'false');
+INSERT INTO article(writer, title, contents, created_at, points, deleted)
+VALUES ('jianId', 'title', '', '20230510', '40', 'false');
+INSERT INTO article(writer, title, contents, created_at, points, deleted)
+VALUES ('jianId', 'title', '', '20230511', '41', 'false');
+INSERT INTO article(writer, title, contents, created_at, points, deleted)
+VALUES ('jianId', 'title', '', '20230512', '42', 'false');
+INSERT INTO article(writer, title, contents, created_at, points, deleted)
+VALUES ('jianId', 'title', '', '20230513', '43', 'false');
+INSERT INTO article(writer, title, contents, created_at, points, deleted)
+VALUES ('jianId', 'title', '', '20230514', '44', 'false');
+INSERT INTO article(writer, title, contents, created_at, points, deleted)
+VALUES ('jianId', 'title', '', '20230515', '45', 'false');
+INSERT INTO article(writer, title, contents, created_at, points, deleted)
+VALUES ('jianId', 'title', '', '20230516', '46', 'false');
+INSERT INTO article(writer, title, contents, created_at, points, deleted)
+VALUES ('jianId', 'title', '', '20230517', '47', 'false');
+INSERT INTO article(writer, title, contents, created_at, points, deleted)
+VALUES ('jianId', 'title', '', '20230518', '48', 'false');
+INSERT INTO article(writer, title, contents, created_at, points, deleted)
+VALUES ('jianId', 'title', '', '20230519', '49', 'false');
+INSERT INTO article(writer, title, contents, created_at, points, deleted)
+VALUES ('jianId', 'title', '', '20230520', '50', 'false');
+INSERT INTO article(writer, title, contents, created_at, points, deleted)
+VALUES ('jianId', 'title', '', '20230521', '51', 'false');
+INSERT INTO article(writer, title, contents, created_at, points, deleted)
+VALUES ('jianId', 'title', '', '20230522', '52', 'false');
+INSERT INTO article(writer, title, contents, created_at, points, deleted)
+VALUES ('jianId', 'title', '', '20230523', '53', 'false');
+INSERT INTO article(writer, title, contents, created_at, points, deleted)
+VALUES ('jianId', 'title', '', '20230524', '54', 'false');
+INSERT INTO article(writer, title, contents, created_at, points, deleted)
+VALUES ('jianId', 'title', '', '20230525', '55', 'false');
+INSERT INTO article(writer, title, contents, created_at, points, deleted)
+VALUES ('jianId', 'title', '', '20230526', '56', 'false');
+INSERT INTO article(writer, title, contents, created_at, points, deleted)
+VALUES ('jianId', 'title', '', '20230527', '57', 'false');
+INSERT INTO article(writer, title, contents, created_at, points, deleted)
+VALUES ('jianId', 'title', '', '20230528', '58', 'false');
+INSERT INTO article(writer, title, contents, created_at, points, deleted)
+VALUES ('jianId', 'title', '', '20230529', '59', 'false');
+INSERT INTO article(writer, title, contents, created_at, points, deleted)
+VALUES ('jianId', 'title', '', '20230530', '60', 'false');
+INSERT INTO article(writer, title, contents, created_at, points, deleted)
+VALUES ('jianId', 'title', '', '20230601', '61', 'false');
+INSERT INTO article(writer, title, contents, created_at, points, deleted)
+VALUES ('jianId', 'title', '', '20230602', '62', 'false');
+INSERT INTO article(writer, title, contents, created_at, points, deleted)
+VALUES ('jianId', 'title', '', '20230603', '63', 'false');
+INSERT INTO article(writer, title, contents, created_at, points, deleted)
+VALUES ('jianId', 'title', '', '20230604', '64', 'false');
+INSERT INTO article(writer, title, contents, created_at, points, deleted)
+VALUES ('jianId', 'title', '', '20230605', '65', 'false');
+INSERT INTO article(writer, title, contents, created_at, points, deleted)
+VALUES ('jianId', 'title', '', '20230606', '66', 'false');
+INSERT INTO article(writer, title, contents, created_at, points, deleted)
+VALUES ('jianId', 'title', '', '20230607', '67', 'false');
+INSERT INTO article(writer, title, contents, created_at, points, deleted)
+VALUES ('jianId', 'title', '', '20230608', '68', 'false');
+INSERT INTO article(writer, title, contents, created_at, points, deleted)
+VALUES ('jianId', 'title', '', '20230609', '69', 'false');
+INSERT INTO article(writer, title, contents, created_at, points, deleted)
+VALUES ('jianId', 'title', '', '20230610', '70', 'false');
+INSERT INTO article(writer, title, contents, created_at, points, deleted)
+VALUES ('jianId', 'title', '', '20230611', '71', 'false');
+INSERT INTO article(writer, title, contents, created_at, points, deleted)
+VALUES ('jianId', 'title', '', '20230612', '72', 'false');
+INSERT INTO article(writer, title, contents, created_at, points, deleted)
+VALUES ('jianId', 'title', '', '20230613', '73', 'false');
+INSERT INTO article(writer, title, contents, created_at, points, deleted)
+VALUES ('jianId', 'title', '', '20230614', '74', 'false');
+INSERT INTO article(writer, title, contents, created_at, points, deleted)
+VALUES ('jianId', 'title', '', '20230615', '75', 'false');
+INSERT INTO article(writer, title, contents, created_at, points, deleted)
+VALUES ('jianId', 'title', '', '20230616', '76', 'false');
+INSERT INTO article(writer, title, contents, created_at, points, deleted)
+VALUES ('jianId', 'title', '', '20230617', '77', 'false');
